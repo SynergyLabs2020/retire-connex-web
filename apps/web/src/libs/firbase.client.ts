d7cece21b7572751190d352f132a966f26d3fb64
ozodbek-synergylab <ozodbek@synergylabs.co>
2025-10-10 14:58:15 +0500

    fix: firbase credential fixes

--- apps/web/src/libs/firbase.client.ts
+++ apps/web/src/libs/firbase.client.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyD-0vnKAOUlpfajKoMN360sYpl5frZAbVM',
-    authDomain: 'retire-connex.firebaseapp.com',
-    projectId: 'retire-connex',
-    storageBucket: 'retire-connex.firebasestorage.app',
-    messagingSenderId: '337393746761',
-    appId: '1:337393746761:web:d2dd4df20274bbf6585098',
-    measurementId: 'G-RGY3NS08Q8',
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_MESSAGING_SENDING_ID,
+    appId: process.env.NEXT_PUBLIC_APPI_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASURMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
